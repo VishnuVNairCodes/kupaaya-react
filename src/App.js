ac334d779cf4a1b2d2e6e20475f945ce1d8c9aa8
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-06-16 07:51:38 +0530

    feat: added Routes for cart, wishlist, login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
+
+import Cart from "./pages/Cart/Cart";
 import HomePage from "./pages/HomePage/HomePage";
+import Login from "./pages/Login/Login";
 import Products from "./pages/Products/Products";
+import Wishlist from "./pages/Wishlist/Wishlist";
 
 const App = () => {
   return (
@@ -9,6 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
