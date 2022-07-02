3129692006fc6bdd3deb7424922c5bf1825114ef
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-07-02 10:07:45 +0530

    added different categories and images to home

--- src/pages/HomePage/components/ProductDisplay1.js
+++ src/pages/HomePage/components/ProductDisplay1.js
@@ -6,43 +6,43 @@ const ProductDisplay1 = () => {
       <div className="display-1">
         <div className="display-1-img-container">
           <img
-            src={require("../../../assets/cute-baby.jpg")}
+            src={require("../../../assets/boy-fashion.jpg")}
             alt="product"
             className="display-1-img img-responsive"
           />
-          <div className="display-1-img-text">Baby's</div>
+          <div className="display-1-img-text">Boy Fashion</div>
         </div>
         <div className="display-1-img-container">
           <img
-            src={require("../../../assets/cute-baby.jpg")}
+            src={require("../../../assets/girl-fashion.jpg")}
             alt="product"
             className="display-1-img img-responsive"
           />
-          <div className="display-1-img-text">Baby's</div>
+          <div className="display-1-img-text">Girl Fashion</div>
         </div>
         <div className="display-1-img-container">
           <img
-            src={require("../../../assets/cute-baby.jpg")}
+            src={require("../../../assets/baby-shoes.jpg")}
             alt="product"
             className="display-1-img img-responsive"
           />
-          <div className="display-1-img-text">Baby's</div>
+          <div className="display-1-img-text">Baby Shoes</div>
         </div>
         <div className="display-1-img-container">
           <img
-            src={require("../../../assets/cute-baby.jpg")}
+            src={require("../../../assets/swagger-kid.jpg")}
             alt="product"
             className="display-1-img img-responsive"
           />
-          <div className="display-1-img-text">Baby's</div>
+          <div className="display-1-img-text">Swagger Kid</div>
         </div>
         <div className="display-1-img-container">
           <img
-            src={require("../../../assets/cute-baby.jpg")}
+            src={require("../../../assets/toy.jpg")}
             alt="product"
             className="display-1-img img-responsive"
           />
-          <div className="display-1-img-text">Baby's</div>
+          <div className="display-1-img-text">Toys</div>
         </div>
       </div>
     </>
