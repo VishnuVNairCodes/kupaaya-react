479867637bc7c869fe970d0de914d10ab6ed81e3
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-01-09 16:54:16 +0530

    style: update variable names to match latest updates of minnal ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import { App } from "./App";
 import { makeServer } from "./server";
 import { FilterProvider } from "./contexts/filter-context";
