dc72c4238d1bebf65f8d9bc1b5d84f11c36f36a1
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-01-30 11:32:08 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-
 import "./App.css";
-
 import { Cart } from "./pages/Cart/Cart";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { Login } from "./pages/Login/Login";
